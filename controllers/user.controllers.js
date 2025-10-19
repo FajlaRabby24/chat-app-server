4b25e6aa79203a5a1549ff38b708ff56f3ac943f
FajlaRabby24 <fajlarabbyla24@gmail.com>
2025-10-19 23:49:41 +0600

    login controllers created

--- controllers/user.controllers.js
+++ controllers/user.controllers.js
@@ -4,9 +4,8 @@ import UserModel from "../models/user.model";
 
 // sign up new user
 export const signup = async (req, res) => {
-  const { fullName, email, password, bio } = req.body;
-
   try {
+    const { fullName, email, password, bio } = req.body;
     if (!fullName || !email || !password || !bio) {
       return res.json({ success: false, message: "Missgin details" });
     }
@@ -33,6 +32,34 @@ export const signup = async (req, res) => {
       userData: newUser,
       token,
       message: "Account created successfully",
+    });
+  } catch (error) {
+    console.log(error.message);
+    res.json({
+      success: false,
+      message: error.message,
+    });
+  }
+};
+
+// login a user
+export const login = async (req, res) => {
+  try {
+    const { email, password } = req.body;
+    const userData = await UserModel.findOne({ email });
+
+    const isPassCorrect = await bcrypt.compare(password, userData.password);
+    if (!isPassCorrect) {
+      return res.json({ success: false, message: "Invalid credentials" });
+    }
+
+    const token = generateToken(newUser._id);
+
+    res.json({
+      success: true,
+      userData: userData,
+      token,
+      message: "Login successfully",
     });
   } catch (error) {
     console.log(error.message);
