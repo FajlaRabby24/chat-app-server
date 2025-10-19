90c82c1d312ed040faa80a9116fd08f3dd5cdf6f
FajlaRabby24 <fajlarabbyla24@gmail.com>
2025-10-20 00:01:13 +0600

    auth middleware added

---
+++ middleware/auth.js
@@ -0,0 +1,25 @@
+import jwt from "jsonwebtoken";
+import UserModel from "../models/user.model";
+
+// verify user
+export const protectRoute = async (req, res, next) => {
+  try {
+    const token = req.headers.token;
+    if (!token) {
+      return res.json({ success: false, message: "Missing token" });
+    }
+
+    const decoded = jwt.verify(token, process.env.JWT_SECRET);
+    const user = await UserModel.findById(decoded.userId).select("-password");
+
+    if (!user) {
+      return res.json({ success: false, message: "User not found" });
+    }
+
+    req.user = user;
+    next();
+  } catch (error) {
+    console.log(error.message);
+    res.json({ success: false, message: error.message });
+  }
+};
