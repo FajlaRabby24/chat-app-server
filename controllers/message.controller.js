7d4943b15574b9124c43f5fa3eed780cf6273797
FajlaRabby24 <fajlarabbyla24@gmail.com>
2025-10-20 17:07:48 +0600

    created api to mark message as seen using message id

--- controllers/message.controller.js
+++ controllers/message.controller.js
@@ -37,7 +37,7 @@ export const getMessage = async (req, res) => {
     const { id: selectedUserId } = req.params;
     const myId = req.user._id;
 
-    const message = await MessageModel.find({
+    const messages = await MessageModel.find({
       $or: [
         { senderId: myId, receiverId: selectedUserId },
         { senderId: selectedUserId, receiverId: myId },
@@ -49,9 +49,21 @@ export const getMessage = async (req, res) => {
       { seen: true }
     );
 
-    res.json({ success: true, message });
+    res.json({ success: true, messages });
   } catch (error) {
     console.error(error.message);
+    res.json({ success: false, message: error.message });
+  }
+};
+
+// api to mark message as seen using message id
+export const markMessageAsSeen = async (req, res) => {
+  try {
+    const { id } = req.params;
+    await MessageModel.findByIdAndUpdate(id, { seen: true });
+    res.json({ success: true });
+  } catch (error) {
+    console.log(error.message);
     res.json({ success: false, message: error.message });
   }
 };
