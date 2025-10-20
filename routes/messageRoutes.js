b74b2477ab5b0e3666f80267118f57ab98a7c2f0
FajlaRabby24 <fajlarabbyla24@gmail.com>
2025-10-20 23:04:41 +0600

    add send/:id route on message route

--- routes/messageRoutes.js
+++ routes/messageRoutes.js
@@ -3,6 +3,7 @@ import {
   getMessage,
   getUsersFroSidebar,
   markMessageAsSeen,
+  sendMessage,
 } from "../controllers/message.controller.js";
 import { protectRoute } from "../middleware/auth.js";
 
@@ -11,5 +12,6 @@ const messageRouter = express.Router();
 messageRouter.get("/users", protectRoute, getUsersFroSidebar);
 messageRouter.get("/:id", protectRoute, getMessage);
 messageRouter.put("/mark/:id", protectRoute, markMessageAsSeen);
+messageRouter.post("/send/:id", protectRoute, sendMessage);
 
 export default messageRouter;
